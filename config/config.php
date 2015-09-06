ee043a33f6b0c7ca7cde720934ddb14608a67233
Marcos Soares <marcos.hoo@gmail.com>
2015-09-06 18:56:58 -0300

    Configuration file fix

--- config/config.php
+++ config/config.php
@@ -6,81 +6,82 @@ return [
     | Template File Extensions
     |--------------------------------------------------------------------------
     */
-    
+
     'extensions' => [ 'html', 'xhtml' ],
-    
+
     /*
     |--------------------------------------------------------------------------
     | Template Output Filters
     |--------------------------------------------------------------------------
     */
-    
+
     'preFilters' => [],
     'postFilters' => [],
-    
+
     /*
     |--------------------------------------------------------------------------
     | Template Encoding
     |--------------------------------------------------------------------------
     */
-    
+
     'encoding' => 'UTF-8',
-    
+
     /*
     |--------------------------------------------------------------------------
     | Template Output Mode
     |--------------------------------------------------------------------------
     */
-    
+
     'outputMode' => PHPTAL::HTML5,
-    
+
     /*
     |--------------------------------------------------------------------------
-    | PHP Code Destination 
+    | PHP Code Destination
     |--------------------------------------------------------------------------
     */
-    
-    'phpCodeDestination' => ( 
-            
-         app('config')->get('view.compiled') 
-             ? app('config')->get('view.compiled') 
+
+    'phpCodeDestination' => (
+
+         app('config')->get('view.compiled')
+             ? app('config')->get('view.compiled')
              : realpath(storage_path('framework/views'))
-            
+
     ),
-    
+
     /*
     |--------------------------------------------------------------------------
     | Force Reparse Template
     |--------------------------------------------------------------------------
     */
-    
+
     'forceReparse' => env('APP_DEBUG',true),
-    
+
     /*
     |--------------------------------------------------------------------------
     | Template Repositories
     |--------------------------------------------------------------------------
     */
-    
-    'templateRepositories' => ( 
-            
+
+    'templateRepositories' => array_map(
+         function ($dir) {
+             return $dir.(defined('TEMPLATE_ID') ? '/' . TEMPLATE_ID : '');
+         },
          app('config')->get('view.paths')
              ? app('config')->get('view.paths')
              : base_path('/resources/views')
-            
-    ) . (defined('TEMPLATE_ID') ? '/' . TEMPLATE_ID : ''),
-    
+    ),
+
     /*
     |--------------------------------------------------------------------------
     | Translation Properties
     |--------------------------------------------------------------------------
     */
-    
+
     'translationClass' => \MarcosHoo\LaravelPHPTAL\PHPTALTranslator::class,
     'translationPath' => base_path('resources/lang/'),
-    'translationFilename' => 'messages',    
+    'translationFilename' => 'messages',
     'translationLanguages' => [
         app('config')->get('app.locale'),
     ]
-    
+
 ];
